3f4a0a85c81986959b164c456a9b9461422f399a
Mehmet KURT <mehmet.kurt@outlook.com>
2023-03-20 20:25:11 +0300

    seperate any partial

---
+++ src/CodeIsLife/Components/SearchViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeIsLife.Components;
+
+public class SearchViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.FromResult(View());
+    }
+}
